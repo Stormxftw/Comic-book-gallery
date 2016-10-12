07c08c7b05c66a5b1dee711b4b97fd27b6634d27
Joe Shaw <Josephshawxx@gmail.com>
2016-10-12 19:03:54 -0400

    added comic book detail view

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,12 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            } 
-            return Content("Hello From the Comic book store!");
-           
+            return View();
         }
     }
 }
